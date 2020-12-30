8b60de2bda2208400ac133f832ec8e925a9cbbf6
Priyanka Sadagopan <priyanka.ks2023@gmail.com>
2020-12-30 02:59:42 +0000

    fixed up jisung's gif in biases because i liked his selfie one better lmao. also added headers for i am who --> miroh

--- script.js
+++ script.js
@@ -15,5 +15,17 @@ function mixtapepage(){
 }
 
 function iamnotpage(){
-  window.location.href = "iamnot.html"
+  window.location.href = "iamnot.html";
+}
+
+function iamwhopage(){
+  window.location.href = "iamwho.html";
+}
+
+function iamyoupage(){
+  window.location.href = "iamyou.html";
+}
+
+function mirohpage(){
+  window.location.href = "miroh.html";
 }
